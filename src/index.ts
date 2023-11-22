3d747edc0638f67b14c87d7df76c11384a099d1b
DynnoOttu <dynnoottu968@gmail.com>
2023-11-23 00:01:22 +0800

    get authors

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import * as dotenv from "dotenv";
 import express from "express"
 import cors from "cors"
 
+import { authorRouter } from "./author/author.router";
+
 dotenv.config()
 
 if(!process.env.PORT){
@@ -13,8 +15,8 @@ const port: number = parseInt(process.env.PORT as string, 10)
 const app = express()
 
 app.use(cors())
-
 app.use(express.json())
+app.use("/api/author", authorRouter)
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`)
